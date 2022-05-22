fd91d95f1f8f58ca420bb59a767bb05b55dc338c
beitaandbaozi <2280496040@qq.com>
2022-05-22 15:47:33 +0800

    feat:ColumnList组件

---
+++ zhihu/src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="item in list" :key="item.id">
+      <img :src="item.avatar" :alt="item.title" />
+      <h5>{{ item.title }}</h5>
+      <p>{{ item.description }}</p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number
+  title: string
+  avatar: string
+  description: string
+}
+export default defineComponent({
+  name: 'ColumnList',
+  components: {},
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped></style>
