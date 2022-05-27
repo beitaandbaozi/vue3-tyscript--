0598b5549bb9d320ad21e7bef4a4a20852e1ecc0
beitaandbaozi <2280496040@qq.com>
2022-05-28 00:29:43 +0800

    feat:封装loading组件

--- zhihu/src/App.vue
+++ zhihu/src/App.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <!-- 头部 -->
     <global-header :user="userData"></global-header>
+    <loading text="加载中" background="rgba(0,0,0,0.8)"></loading>
     <!-- 内容 -->
     <router-view></router-view>
     <!-- 底部 -->
@@ -15,12 +16,14 @@ import GlobalHeader from './components/GlobalHeader.vue'
 import GlobalFooter from './components/GlobalFooter.vue'
 import { useStore } from 'vuex'
 import { GlobalDataProps } from './store'
+import Loading from './components/Loading.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     GlobalHeader,
-    GlobalFooter
+    GlobalFooter,
+    Loading
   },
   setup () {
     const store = useStore<GlobalDataProps>()
---
+++ zhihu/src/components/Loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="d-flex justify-content-center align-items-center h-100 loading-container"
+    :style="
+       {
+        backgroundColor: background || ''
+      }
+    "
+  >
+    <div class="loading-content">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">{{ text || 'loading'}}</span>
+      </div>
+      <p class="text-primary small" v-if="text">{{ text }}</p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Loading',
+  props: {
+    text: {
+      type: String
+    },
+    background: {
+      type: String
+    }
+  }
+})
+</script>
+<style>
+.loading-container {
+  background: rgba(255, 255, 255, 0.5);
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  text-align: center;
+}
+</style>
