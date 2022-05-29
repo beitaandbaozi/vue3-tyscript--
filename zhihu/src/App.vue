054d29227fed0b3ebd49134efdb065074d333400
beitaandbaozi <2280496040@qq.com>
2022-05-29 12:47:28 +0800

    feat:封装Message组件

--- zhihu/src/App.vue
+++ zhihu/src/App.vue
@@ -7,6 +7,7 @@
       background="rgba(0,0,0,0.8)"
       v-if="loading"
     ></loading>
+    <message message="Message"></message>
     <!-- 内容 -->
     <router-view></router-view>
     <!-- 底部 -->
@@ -22,13 +23,15 @@ import { useStore } from 'vuex'
 import { GlobalDataProps } from './store'
 import Loading from './components/Loading.vue'
 import axios from 'axios'
+import Message from './components/Message.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     GlobalHeader,
     GlobalFooter,
-    Loading
+    Loading,
+    Message
   },
   setup () {
     const store = useStore<GlobalDataProps>()
---
+++ zhihu/src/components/Message.vue
@@ -0,0 +1,69 @@
+<template>
+  <teleport to="#message">
+    <div
+      class="alert message-info fixed-top w-50 mx-auto d-flex justify-content-between mt-2 fade show"
+      :class="classObj"
+      role="alert"
+      v-if="isVisible"
+    >
+      <strong>{{ message }}</strong>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        @click.prevent="hide"
+      ></button>
+    </div>
+  </teleport>
+</template>
+<script lang="ts">
+import { defineComponent, onUnmounted, PropType, ref } from 'vue'
+type messageType =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'dark'
+export default defineComponent({
+  name: 'Message',
+  props: {
+    type: {
+      type: String as PropType<messageType>,
+      default: 'primary'
+    },
+    message: String
+  },
+  emits: ['close-message'],
+  setup (props, context) {
+    const node = document.createElement('div')
+    node.id = 'message'
+    document.body.appendChild(node)
+    onUnmounted(() => {
+      document.body.removeChild(node)
+    })
+
+    const classObj = {
+      'alert-primary': props.type === 'primary',
+      'alert-secondary': props.type === 'secondary',
+      'alert-success': props.type === 'success',
+      'alert-danger': props.type === 'danger',
+      'alert-warning': props.type === 'warning',
+      'alert-info': props.type === 'info',
+      'alert-dark': props.type === 'dark'
+    }
+    const isVisible = ref(true)
+    const hide = () => {
+      isVisible.value = false
+      context.emit('close-message', true)
+    }
+    return {
+      classObj,
+      isVisible,
+      hide
+    }
+  }
+})
+</script>
